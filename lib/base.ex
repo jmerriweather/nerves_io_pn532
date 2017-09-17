f70bdf53ad521a486a80287de2b49ec3d1bf541e
Jonathan Merriweather <Jono@merriweather.com.au>
2017-09-17 15:00:39 +1000

    change task to start_link

--- lib/base.ex
+++ lib/base.ex
@@ -327,7 +327,7 @@ defmodule Nerves.IO.PN532.Base do
       end
 
       def handle_info(:setup, state) do    
-        Task.start(__MODULE__, :setup, [self(), state])
+        Task.start_link(__MODULE__, :setup, [self(), state])
 
         {:noreply, state}
       end
